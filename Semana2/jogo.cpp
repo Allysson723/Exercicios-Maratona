f404133e2184d1ea845c173f178d55130790ad85
anybody723 <anybody723@proton.me>
2024-03-22 14:27:18 -0300

    jogo completo, teste wifi

--- Semana2/jogo.cpp
+++ Semana2/jogo.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int N;
+    cin >> N;
+    cout << (N + 1) * (N + 2) / 2;
     return 0;
 }
--- Semana2/wifi.cpp
+++ Semana2/wifi.cpp
@@ -1,3 +1,33 @@
-//
-// Created by anybody on 20/03/24.
-//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int salas[N][4];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> salas[i][j];
+        }
+    }
+    int roteadores = 0;
+    for (int i = 0; i < N; i++) {
+        bool contido = false;
+        for (int j = 0; j < N; j++) {
+            if (i != j &&
+                salas[i][0] >= salas[j][0] &&
+                salas[i][1] <= salas[j][1] &&
+                salas[i][2] <= salas[j][2] &&
+                salas[i][3] >= salas[j][3]) {
+                contido = true;
+                break;
+            }
+        }
+        if (!contido) {
+            roteadores++;
+        }
+    }
+    cout << roteadores;
+    return 0;
+}
