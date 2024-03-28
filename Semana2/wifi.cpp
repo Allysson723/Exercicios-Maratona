fa40b990f58aebdf42747dda96fa44a2dff76f81
anybody723 <anybody723@proton.me>
2024-03-28 13:28:58 -0300

    wifi 20/100

--- Semana2/wifi.cpp
+++ Semana2/wifi.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-struct Sala {
-    int x1, y1, x2, y2, area;
+class Sala {
+private:
+    int x1, y1, x2, y2, per;
+
+public:
+    Sala(int x1, int y1, int x2, int y2) : x1(x1), y1(y1), x2(x2), y2(y2) {
+        this->per = abs((this->x2 - this->x1) + (this->y1 - this->y2));
+    }
+    bool operator<(const Sala& other) const {
+        return per < other.per;
+    }
+    bool isDentro(const Sala& other) const {
+        return (x1 > other.x1 && y1 < other.y1 && x2 < other.x2 && y2 > other.y2);
+    }
 };
 
-bool sala2ContemSala1(const Sala &sala1, const Sala &sala2) {
-    return sala1.x1 >= sala2.x1 && sala1.y1 <= sala2.y1 && sala1.x2 <= sala2.x2 && sala1.y2 >= sala2.y2;
-}
-
-bool comparaSalaPorArea(const Sala &sala1, const Sala &sala2) {
-    return sala1.area < sala2.area;
-}
-
 int main() {
-    int N;
-    cin >> N;
-    vector<Sala> salas(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> salas[i].x1 >> salas[i].y1 >> salas[i].x2 >> salas[i].y2;
-        salas[i].area = abs(salas[i].x2 - salas[i].x1) * abs(salas[i].y2 - salas[i].y1);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int qSalas;
+    cin >> qSalas;
+    vector<Sala> salas;
+    for (int i = 0; i < qSalas; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        salas.push_back(Sala(x1, y1, x2, y2));
     }
-    sort(salas.begin(), salas.end(), comparaSalaPorArea);
-    for (int i = 0; i < N - 1; i++) {
-        for (int j = i + 1; j < N; j++) {
-            if (sala2ContemSala1(salas[i], salas[j])) {
+    sort(salas.begin(), salas.end());
+    for (size_t i = 0; i < salas.size(); i++) {
+        for (size_t j = i + 1; j < salas.size(); j++) {
+            if (salas[i].isDentro(salas[j])) {
                 salas.erase(salas.begin() + j);
-                N--;
                 j--;
             }
         }
     }
-    cout << salas.size();
+    cout << salas.size() << endl;
     return 0;
 }
