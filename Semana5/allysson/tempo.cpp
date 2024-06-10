e7967d9d4f1f448064919b59db460905c6c23fc8
Allysson723 <0063398@academico.ifmg.edu.br>
2024-06-10 07:46:58 -0300

    tempo

--- Semana5/allysson/tempo.cpp
+++ Semana5/allysson/tempo.cpp
@@ -7,43 +7,47 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    vector<pair<char, int>> registros;
     map<int, pair<int, bool>> amigos;
-
+    char CAnterior;
     for (int i = 0; i < N; ++i) {
         char C;
         int I;
         cin >> C >> I;
-        registros.push_back({C, I});
         if (C != 'T') {
+            //caso o amigo não esteja na lista ele é adicionado
             if (amigos.find(I) == amigos.end()) {
-                amigos[I] = {0, true};
+                amigos[I] = {0, false};
             }
-        }
-    }
-    amigos[registros[0].second].second = false;
-    for (int i = 1; i < N; ++i) {
-        if (registros[i].first != 'T') {
-            if (registros[i].first == 'E') {
-                // Alterando o estado de resposta do amigo para true
-                amigos[registros[i].second].second = true;
-            }else{
-                amigos[registros[i].second].second = false;
-            }
-            // Adicionando 1 ao tempo de resposta de todos os amigos não respondidos
-            for (auto &amigo: amigos) {
-                if (!amigo.second.second && amigo.first != registros[i].second) {
-                    amigo.second.first++;
+            if (C == 'R' && i != 0) {
+                amigos[I].second = false;
+                //adicionando 1 ao tempo de resposta de todos os amigos não respondidos
+                if (CAnterior != 'T') {
+                    for (auto &amigo: amigos) {
+                        if (!amigo.second.second && amigo.first != I) {
+                            amigo.second.first++;
+                        }
+                    }
                 }
+            } else if (i != 0) {
+                if (CAnterior != 'T') {
+                    for (auto &amigo: amigos) {
+                        if (!amigo.second.second) {
+                            amigo.second.first++;
+                        }
+                    }
+                }
+                amigos[I].second = true;
             }
         } else {
-            // Adicionando o tempo do registro ao tempo de resposta de todos os amigos não respondidos
-            for (auto &amigo: amigos) {
-                if (!amigo.second.second) {
-                    amigo.second.first += registros[i].second;
+            if (CAnterior != 'T') {
+                for (auto &amigo: amigos) {
+                    if (!amigo.second.second) {
+                        amigo.second.first += I;
+                    }
                 }
             }
         }
+        CAnterior = C;
     }
     for (const auto &amigo: amigos) {
         // Se o amigo não tiver respondido a todas as mensagens, imprime -1 como tempo de resposta total
