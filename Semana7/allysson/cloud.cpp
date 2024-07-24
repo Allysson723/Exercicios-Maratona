ff3c1ab87a1b2d853dd67eb624b812cca11b042e
Allysson723 <0063398@academico.ifmg.edu.br>
2024-07-24 12:15:27 -0300

    começo do cloud

--- Semana7/allysson/cloud.cpp
+++ Semana7/allysson/cloud.cpp
@@ -1,3 +1,36 @@
-//
-// Created by anybody on 23/07/24.
-//
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    while(N != M && N != 0){
+        unordered_map<int, vector<string>> servidores;
+        unordered_map<int, vector<string>> clientes;
+        for (int i = 0; i < N; i++) {
+            int S;
+            cin >> S;
+            for (int j = 0; j < S; j++) {
+                string A;
+                cin >> A;
+                servidores[S].push_back(A);
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            int C;
+            cin >> C;
+            for (int j = 0; j < C; j++) {
+                string A;
+                cin >> A;
+                clientes[C].push_back(A);
+            }
+        }
+        cin >> N >> M;
+    }
+    return 0;
+}
