8e4178fceec3e4f84f89bc439af1760f41869c78
anybody723 <anybody723@proton.me>
2024-07-31 22:18:15 -0300

    semana 8 concluída e otimizada

---
+++ cmake-build-debug/CMakeFiles/vagoes.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vagoes.dir/Semana8/vagoes.cpp.obj"
+  "CMakeFiles/vagoes.dir/Semana8/vagoes.cpp.obj.d"
+  "libvagoes.dll.a"
+  "vagoes.exe"
+  "vagoes.exe.manifest"
+  "vagoes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vagoes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/vagoes.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vagoes.
+# This may be replaced when dependencies are built.
